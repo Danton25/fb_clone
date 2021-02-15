90a8e93c6636b727e184332465f08d099fcfd901
Danton25 <neekhilyaadav25@gmail.com>
2021-02-14 19:05:30 -0500

    basic setup

---
+++ fb-clone/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build</h1>
+
+      {/* header */}
+      {/* body */}
+        {/* sidebar */}
+        {/* feed */}
+        {/* widgets */}
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAGKPmMqa-Cr0GhoOOEC-FzRGutQZCOknw",
+    authDomain: "facebook-clone-2df67.firebaseapp.com",
+    projectId: "facebook-clone-2df67",
+    storageBucket: "facebook-clone-2df67.appspot.com",
+    messagingSenderId: "403957560176",
+    appId: "1:403957560176:web:db43ed51ac38c4d91bd5b5",
+    measurementId: "G-EL98K3MMVB"
+  };
