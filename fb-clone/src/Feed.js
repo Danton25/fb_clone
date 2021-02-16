8f4e9538870cb244f6237bd9c0c1002627e9ce4e
Danton25 <neekhilyaadav25@gmail.com>
2021-02-16 01:44:36 -0500

    Left facebook pane complete

---
+++ fb-clone/src/Feed.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import StoryReel from './StoryReel'
+function Feed() {
+    return (
+        <div className = "feed">
+            <StoryReel />
+            {/* messageSender */}
+                {/* info */}
+                    {/* postinfo */}
+                    {/* postphoto */}
+                {/* activity */}
+                    {/* entbar */}
+
+        </div>
+    )
+}
+
+export default Feed
